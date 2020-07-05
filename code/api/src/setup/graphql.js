8d0d6cd35c7a088d1f30e037748bae9b4306f31b
Ryan Camp <campryan@comcast.net>
2020-07-05 10:34:28 -0600

    Annotate setup/graphql.js

--- code/api/src/setup/graphql.js
+++ code/api/src/setup/graphql.js
@@ -1,5 +1,9 @@
+// This file sets up GraphQL endpoint, it imports configuration settings, schema,
+// and authentication from other files and exports itself to index.js.
+// We won't need to change anything in this file - Ryan
+
 // Imports
-import graphqlHTTP from 'express-graphql'
+import graphqlHTTP from 'express-graphql' //<-- this is needed to set up graphql endpoint. - Ryan
 
 // App Imports
 import serverConfig from '../config/server.json'
@@ -10,13 +14,13 @@ import schema from './schema'
 export default function (server) {
   console.info('SETUP - GraphQL...')
 
-  server.use(authentication)
+  server.use(authentication) // <-- Authentication, We don't need to worry about this!! - Ryan
 
   // API (GraphQL on route `/`)
   server.use(serverConfig.graphql.endpoint, graphqlHTTP(request => ({
-    schema,
-    graphiql: serverConfig.graphql.ide,
-    pretty: serverConfig.graphql.pretty,
+    schema, //<-- Sets schema for graphql - Ryan
+    graphiql: serverConfig.graphql.ide, // <-- Ensures the graphiql can be accessed through endpoint. - Ryan
+    pretty: serverConfig.graphql.pretty,// <-- pretty print json response - Ryan
     context: {
       auth: {
         user: request.user,
