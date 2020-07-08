aff6bf770fa748dd2763051a8f00f00cca2270fb
Margo Flewelling <flewelling.margo@gmail.com>
2020-07-08 15:24:40 -0600

    Successfully add to database and clear after all tests

--- code/api/src/__tests__/modules/users/query_test.js
+++ code/api/src/__tests__/modules/users/query_test.js
@@ -3,16 +3,15 @@ import schema from '../../../setup/schema';
 import graphqlHTTP from 'express-graphql';
 import request from 'supertest';
 import model from '../../../setup/models';
-import bcrypt from 'bcrypt'
-import config from '../../../config/server';
-import sequelize from 'sequelize'
-
+import bcrypt from 'bcrypt';
+import db from '../../../setup/database';
 
 describe ("user queries", () => {
 
 let server = express();
+let admin;
 
-  beforeAll(() => {
+  beforeAll(async() => {
     server.use('/', graphqlHTTP({
       schema,
       graphiql: false
@@ -33,20 +32,15 @@ let server = express();
           createdAt: new Date(),
           updatedAt: new Date()
         };
-    async(done) => {
-      await sequelize.sync();
-      await model.User.bulkInsert([user1, user2]);
-      done();
-    }
-
-    console.log("*************")
-    console.log(model.User.findAll());
-    console.log("*************")
+      admin = await model.User.create(user1);
+      await model.User.create(user2);
   });
 
-  // afterAll(() => {
-  //   model.User.destroy({where: {}})
-  // });
+  afterAll(async() => {
+    await model.User.destroy({ where: {}});
+    db.close();
+    console.log("((((((((((((()))))))))))))")
+  });
 
   it("gets all users", async() => {
     // expect.assertions(2)
@@ -54,18 +48,18 @@ let server = express();
       .get('/')
       .send({ query: '{ users { name } }'})
       .expect(200);
+      console.log(admin.id)
 
-      console.log(response.body)
     // expect(response.body.data.users.length).toEqual(2);
     // expect(response.body.data.users[0].name).toEqual("The Admin");
   });
-
+  //
   // it("gets single user by id", async() => {
   //   const response = await request(server)
   //     .get('/')
-  //     .send({ query: '{ user(id: 1) { name } }'})
+  //     .send({ query: '{ user(id: user1.id) { name } }'})
   //     .expect(200);
-  //
+  //   console.log(response.body.data)
   //   expect(response.body.data.user.name).toEqual("The Admin");
   //   expect(response.body.data.user.name).not.toEqual("The User");
   // });
