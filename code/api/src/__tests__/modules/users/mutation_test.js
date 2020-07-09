4eeb84eb8b2b754e765dd05761a0284ba808d132
Ryan Camp <campryan@comcast.net>
2020-07-09 16:29:03 -0600

    Complete mutation testing for user

--- code/api/src/__tests__/modules/users/mutation_test.js
+++ code/api/src/__tests__/modules/users/mutation_test.js
@@ -1,28 +1,50 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import request from 'supertest';
+import bcrypt from 'bcrypt';
 
 import schema from '../../../setup/schema';
-import db from '../../../setup/database';
 import models from '../../../setup/models'
+import db from '../../../setup/database';
 
 describe ("user mutations", () => {
 
   let server = express();
+  let admin;
+  let user;
 
-  beforeAll(async() => {
+  beforeAll(async () => {
     server.use('/', graphqlHTTP({
       schema,
       graphiql: false
     }));
-  });
 
-  afterAll(async() => {
-    await models.User.destroy({ where: {}});
-    db.close();
+    const user1 = {
+      name: 'The Admin',
+      email: 'admin@crate.com',
+      password: bcrypt.hashSync('123456', 10),
+      role: "ADMIN",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    };
+    const user2 = {
+      name: 'The User',
+      email: 'user@crate.com',
+      password: bcrypt.hashSync('123456', 10),
+      role: "USER",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    };
+
+    admin = await models.User.create(user1);
+    user = await models.User.create(user2);
   })
 
-  it("can signup a user", async() => {
+  afterAll(async () => {
+    await models.User.destroy({ where: {}});
+  });
+
+  test("can signup a user", async() => {
     const response = await request(server)
       .post('/')
       .send({  query: `
@@ -35,6 +57,26 @@ describe ("user mutations", () => {
       .expect(200);
 
       expect(response.body.data.userSignup.name).toEqual("travis");
+  });
+
+  it("can remove a user", async() => {
+    let is_here = await models.User.findOne({where: {id: user.id}});
+    expect(is_here.id).toEqual(user.id)
+
+    const response = await request(server)
+      .post('/')
+      .send({  query: `
+        mutation userRemove {
+          userRemove(id: ${user.id}){
+            name
+          }
+        }
+        `
+       })
+      .expect(200);
+
+     let not_here = await models.User.findOne({where: {id: user.id}});
+     expect(not_here).toBe(null)
   });
 
 });
