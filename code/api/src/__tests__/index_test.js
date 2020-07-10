f918b4fc2ae3e2b51d326117df7aa8f54d0a5d44
Ryan Camp <campryan@comcast.net>
2020-07-09 19:10:36 -0600

    Rename 'users' test file to 'user' to match modules

--- code/api/src/__tests__/index_test.js
+++ code/api/src/__tests__/index_test.js
@@ -2,9 +2,9 @@
 import db from '../setup/database'
 //Tests to Run
 //Run Query Tests 1st
-import query from './modules/users/query_test'
+import query from './modules/user/query_test'
 //Run Mutation Tests 2nd
-import mutation from './modules/users/mutation_test'
+import mutation from './modules/user/mutation_test'
 
 afterAll(() => {
   db.close();
