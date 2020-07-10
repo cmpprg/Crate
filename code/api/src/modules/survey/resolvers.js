b713e725d2f6414fd349a66fbd2c1c818ab1a170
Ryan Camp <campryan@comcast.net>
2020-07-10 07:25:31 -0600

    Start resolver file for survey

---
+++ code/api/src/modules/survey/resolvers.js
@@ -0,0 +1,9 @@
+// App Imports
+import models from '../../setup/models'
+
+// Get subscription by ID
+export async function create(
+  parentValue, {  tops, bottoms, dresses, shoes, accessories }, context
+){
+  return console.log(context)
+}
